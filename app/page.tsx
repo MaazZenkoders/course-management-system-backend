fa8f8d94b712147195229447cf262b126d9ea419
Maaz Mustafa <maaz05.zenkoders@gmail.com>
2024-05-28 21:26:56 +0500

    form

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <h1 className="font-bold text-green-400 text-3xl mt-5 px-16">Course Management System</h1>
+   </main>
+  );
+}
